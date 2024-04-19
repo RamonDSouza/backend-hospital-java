a355a4912b0ed3167bbb9fd4ec97af79e8272fe7
Ramon Duarte <ramon_duarte.10@hotmail.com>
2024-04-19 13:52:25 -0300

    CRUD de medicos

---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,61 @@
+package med.voll.api.medico;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import med.voll.api.endereco.Endereco;
+
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+    private String telefone;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+    private Boolean ativo;
+
+    public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
+        this.endereco = new Endereco(dados.endereco());
+        this.especialidade = dados.especialidade();
+        this.crm = dados.crm();
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+    }
+
+    public void atualizarinformacoes(DadosAtualizacaoMedico dados) {
+        if(dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null){
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
+
+    public void excluir() {
+        this.ativo = false;
+    }
+}
